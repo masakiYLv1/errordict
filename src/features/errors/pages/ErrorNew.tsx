ac389171a06b23a8a1170c518b9a26e0485196bc
m.y <masaki.yamadalv1@gmail.com>
2026-01-07 23:36:01 +0900

    タグinsert 中間テーブルinsert 実装

--- src/features/errors/pages/ErrorNew.tsx
+++ src/features/errors/pages/ErrorNew.tsx
@@ -36,28 +36,63 @@ export const ErrorNew = () => {
     formState: { errors },
   } = useForm<FormData>();
 
-  const handleCreateData = async (data: FormData) => {
+  const handleCreateData = async (insertFormData: FormData) => {
     try {
-      // 参照リンク - データベースに配列で登録するための処理 -
-      const referenceLinks = data.reference_links
+      // errorテーブル 参照リンク - データベースに配列で登録するための処理 -
+      const referenceLinks = insertFormData.reference_links
         .split("\n")
         .map((link) => link.trim())
         .filter(Boolean);
 
-      const { error } = await supabase.from("errors").insert([
-        {
-          title: data.title,
-          message: data.message,
-          situation: data.situation,
-          environment: data.environment,
-          cause: data.cause,
-          solution: data.solution,
-          reference_links: referenceLinks,
-        },
-      ]);
+      // errorテーブル insert
+      const { data: errorId, error: errorMessage } = await supabase
+        .from("errors")
+        .insert([
+          {
+            title: insertFormData.title,
+            message: insertFormData.message,
+            situation: insertFormData.situation,
+            environment: insertFormData.environment,
+            cause: insertFormData.cause,
+            solution: insertFormData.solution,
+            reference_links: referenceLinks,
+          },
+        ])
+        .select("id")
+        .single();
 
-      if (error) {
-        throw error;
+      if (!errorId || errorMessage) {
+        throw errorMessage ?? new Error("Failed to insert error record");
+      }
+
+      // tagsテーブル tagsをSupabaseに登録するための処理
+      const tagsNameToInsert = insertFormData.tags.map((tag) => ({
+        name: tag,
+      }));
+
+      // tagsテーブル upsert
+      const { data: tagsId, error: tagsError } = await supabase
+        .from("tags")
+        .upsert(tagsNameToInsert, { onConflict: "name" })
+        .select("id");
+
+      if (!tagsId || tagsError) {
+        throw tagsError ?? new Error("Faild to upsert tags");
+      }
+
+      // error_tagsテーブル tagsId → tagId 中間テーブル用に定義
+      const errorTagsToInsert = tagsId?.map((tagId) => ({
+        error_id: errorId.id,
+        tag_id: tagId.id,
+      }));
+
+      // error_tagsテーブル 中間テーブル insert
+      const { error: errorTagsError } = await supabase
+        .from("error_tags")
+        .insert(errorTagsToInsert);
+
+      if (errorTagsError) {
+        throw errorTagsError;
       }
 
       toaster.create({
